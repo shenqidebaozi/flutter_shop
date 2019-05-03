554578edadf024078b202b132f04b78d8533566f
shenqidebaozi <906604588@qq.com>
2019-05-03 15:37:53 +0800

    入口文件

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('查券特卖'),
+      ),
+      body: Center(
+        child: Text('查券特卖'),
+      ),
+    );
+  }
+}
